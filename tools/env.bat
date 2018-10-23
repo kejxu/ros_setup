8553f44a423f9ba30d301efacdc4501b7fbf7c39
kejxu <kejxu@microsoft.com>
2018-10-23 01:48:11 -0700

    add local batch files

---
+++ tools/env.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM add python 2.7 path to the PATH variable
+set Dev_PythonPath=c:\opt\python27amd64
+set PATH=%PATH%;%Dev_PythonPath%
+
+REM add python 2.7 Scripts path to the PATH variable
+set Dev_PythonScriptsPath=\Scripts
+set PATH=%PATH%;%Dev_PythonPath%%Dev_PythonScriptsPath%
