7315afb29422bacb7993fc4e4f160847119c4772
James Xu <kejxu@microsoft.com>
2019-02-11 11:27:37 -0800

    add rosdep env batch

--- tools/env.bat
+++ tools/env.bat
@@ -7,3 +7,6 @@ set PATH=%PATH%;%Dev_PythonPath%
 REM add python 2.7 Scripts path to the PATH variable
 set Dev_PythonScriptsPath=\Scripts
 set PATH=%PATH%;%Dev_PythonPath%%Dev_PythonScriptsPath%
+
+REM add rosdep path to the PATH variable
+set PATH=%PATH%;c:\opt\rosdeps\x64\bin
